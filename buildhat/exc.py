651370f5de01c8cd802173395060959abe41c97d
Owen Bishop <owenlbishop@gmail.com>
2025-05-22 13:48:09 -0400

    Missed one

--- buildhat/exc.py
+++ buildhat/exc.py
@@ -1,18 +1,5 @@
 """Exceptions for all build HAT classes"""
 
-
-class DistanceSensorError(Exception):
-    """Error raised when invalid arguments passed to distance sensor functions"""
-
-
-class MatrixError(Exception):
-    """Error raised when invalid arguments passed to matrix functions"""
-
-
-class LightError(Exception):
-    """Error raised when invalid arguments passed to light functions"""
-
-
 class MotorError(Exception):
     """Error raised when invalid arguments passed to motor functions"""
 
